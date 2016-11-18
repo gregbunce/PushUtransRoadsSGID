3628fa09bc822e9833ac67e45d493c7734b095dc
Greg Bunce <gbunce@gmail.com>
2016-11-18 09:52:43 -0700

    Add project files.

---
+++ PushUtransRoadsSGID/clsTlbrUtransRoadsSGID.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.ADF.BaseClasses;
+
+namespace PushUtransRoadsSGID
+{
+    /// <summary>
+    /// Summary description for clsTlbrUtransRoadsSGID.
+    /// </summary>
+    [Guid("66a280f0-67fc-4d49-8da7-c1a0194b3841")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("PushUtransRoadsSGID.clsTlbrUtransRoadsSGID")]
+    public sealed class clsTlbrUtransRoadsSGID : BaseToolbar
+    {
+        #region COM Registration Function(s)
+        [ComRegisterFunction()]
+        [ComVisible(false)]
+        static void RegisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryRegistration(registerType);
+
+            //
+            // TODO: Add any COM registration code here
+            //
+        }
+
+        [ComUnregisterFunction()]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryUnregistration(registerType);
+
+            //
+            // TODO: Add any COM unregistration code here
+            //
+        }
+
+        #region ArcGIS Component Category Registrar generated code
+        /// <summary>
+        /// Required method for ArcGIS Component Category registration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryRegistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommandBars.Register(regKey);
+        }
+        /// <summary>
+        /// Required method for ArcGIS Component Category unregistration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryUnregistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommandBars.Unregister(regKey);
+        }
+
+        #endregion
+        #endregion
+
+        public clsTlbrUtransRoadsSGID()
+        {
+            //
+            // TODO: Define your toolbar here by adding items
+            //
+            //AddItem("esriArcMapUI.ZoomInTool");
+            //BeginGroup(); //Separator
+            //AddItem("{FBF8C3FB-0480-11D2-8D21-080009EE4E51}", 1); //undo command
+            //AddItem(new Guid("FBF8C3FB-0480-11D2-8D21-080009EE4E51"), 2); //redo command
+            AddItem("{2fa3e1b5-eea5-4bd6-860b-31607ca26ce0}");  //push utrans roads to sgid button
+        }
+
+        public override string Caption
+        {
+            get
+            {
+                //TODO: Replace bar caption
+                return "Push UTRANS Roads to SGID";
+            }
+        }
+        public override string Name
+        {
+            get
+            {
+                //TODO: Replace bar ID
+                return "clsTlbrUtransRoadsSGID";
+            }
+        }
+    }
+}
